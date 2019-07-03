e20286fefc82144bddaeec108ea2a10a2ced14a2
정우영 <jgh759123@gmail.com>
2019-07-03 13:17:08 +0900

    Update VoiceDHT11.py
    edit VOICE

--- VoiceDHT11.py
+++ VoiceDHT11.py
@@ -15,15 +15,22 @@ import Adafruit_DHT as dht
 def main():
     file_name = "./testtts.wav"
 
-    humi, temp = dht.read_retry(dht.DHT11, 2)
+    TTS.getText2VoiceStream("온도를 측정하고 있습니다.", file_name)
+    MS.play_file(file_name)
+    humi,temp = dht.read_retry(dht.DHT11, 3)
+
+    TTS.getText2VoiceStream("온도와 습도 중에 원하는 센서값을 알려주세요.", file_name)
+    MS.play_file(file_name)
+
     text = STT.getVoice2Text()
 
     if text.find("온도") >= 0:
-        temp_str = TTS.getText2VoiceStream("현재온도는 {}도 입니다.".format(temp), file_name)
+        TTS.getText2VoiceStream("현재온도는 {}도 입니다.".format(temp), file_name)
         MS.play_file(file_name)
     elif text.find("습도") >= 0:
-        humi_str = TTS.getText2VoiceStream("현재습도는 {}퍼센트 입니다.".format(humi), file_name)
+        TTS.getText2VoiceStream("현재습도는 {}퍼센트 입니다.".format(humi), file_name)
         MS.play_file(file_name)
 
 if __name__ == '__main__':
     main()
+
